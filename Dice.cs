dd168645302b890083695917df086c70805fc5ba
zachbagley <zachbagley@ZACHBAGLEY28EA>
2024-01-17 19:18:36 -0700

    Add project files.

---
+++ Dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class Dice
+    {
+        public int[] roll(int rolls)
+        {
+            int[] rollOutcomes = new int[11];
+            //do as many rolls as specified
+            for (int i = 0; i < rolls; i++)
+            {
+                Random rand = new Random();
+                int diceSum;
+                //roll first die
+                int die1 = rand.Next(1, 7);
+                //roll second die
+                int die2 = rand.Next(1, 7);
+                //calculate result
+                diceSum = die1 + die2;
+                //increment array according to result
+                rollOutcomes[diceSum - 2]++;
+            }
+            return rollOutcomes;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using Mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numRolls;
+        string userInput;
+        bool isInt;
+        int[] results;
+        string resultLine;
+        double rollPct;
+        double totalPct = 0;
+        Dice dr;
+
+
+        //get input
+        Console.WriteLine("Welcome to the dice throwing simulator.");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        userInput = Console.ReadLine();
+
+        //checks if input is integer
+        isInt = int.TryParse(userInput, out int result);
+        if (isInt)
+        {
+            numRolls = int.Parse(userInput);
+            dr = new Dice();
+            results = dr.roll(numRolls);
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numRolls);
+            for (int i = 0; i < results.Length; i++)
+            {
+                rollPct = Math.Round(100 * ((double)results[i] / numRolls));
+                totalPct = totalPct + rollPct;
+                resultLine = (i + 2).ToString() + ": ";
+                for (int j = 0; j < (int)rollPct; j++)
+                {
+                    resultLine = resultLine + "*";
+                }
+                Console.WriteLine(resultLine);
+            }
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+            Console.WriteLine("Total pct for testing: " + totalPct);
+        }
+        else
+        {
+            Console.WriteLine("Error: Input is not a whole number");
+        }
+
+    }
+}
